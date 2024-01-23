a7958673260154966bbabdb2e1c133dc17b11eb4
Bhawesh Chaudhary <thisisbhawesh@gmail.com>
2024-01-23 15:16:09 +0545

    [fix]: Improve Intro Section
    - Add Social Link
    - Add Visuals to Social Icon

--- src/components/MyIntro.tsx
+++ src/components/MyIntro.tsx
@@ -75,10 +75,16 @@ export default function MyIntro() {
           Download CV{" "}
           <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
         </a>
-        <a className="bg-white p-4 text-gray-700 flex items-center gap-2 rounded-full focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer border border-black/10">
+        <a
+          className="bg-white p-4 text-gray-700 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer border border-black/10"
+          href="https://www.linkedin.com/in/thisisbhawesh/"
+        >
           <BsLinkedin />
         </a>
-        <a className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer border border-black/10">
+        <a
+          className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer border border-black/10"
+          href="https://github.com/bhaweshchaudhary"
+        >
           <FaGithubSquare />
         </a>
       </motion.div>
